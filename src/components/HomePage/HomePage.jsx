b360fd7e7a71d5ba497034da4fa758f2c651545d
Dan Kreft <dan@kreft.net>
2020-08-06 14:41:02 -0700

    Moved things around to where I want them, added SASS

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+import styles from './styles.module.sass'
+
+export default function HomePage() {
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        
+      </main>
+
+      <footer className={styles.footer}>
+
+      </footer>
+    </div>
+  )
+}
